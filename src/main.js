9b37e4676eabe7a5c444bc10b7451952a8997f3d
AleksiRasmus <eemilrasmus@gmail.com>
2026-03-11 18:23:16 +0200

    Logging library implementation with Winston

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const logger = require('./logger');
+
+logger.info('This is an informational message.');
+logger.warn('This is a warning message.');
+logger.error('This is an error message.');
+
+logger.info('This is another informational message.');
+logger.warn('This is another warning message.');
+logger.error('This is another error message.');
